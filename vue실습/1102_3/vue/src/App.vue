b479ca256b3d4ef0b9c04633fb5fc7cc0169ea71
parktaesingood <a01091976126@gmail.com>
2022-11-03 07:07:48 +0900

    vue update

---
+++ vue실습/1102_3/vue/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <TodoList :todos="todos" @delete-todo="deleteTodo"/>
+    <TodoForm @create-todo="creataeTodo"/>
+  </div>
+</template>
+
+<script>
+import TodoForm from '@/components/TodoForm.vue'
+import TodoList from '@/components/TodoList.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    TodoForm,
+    TodoList,
+  },
+  data: function () {
+    return{
+      todos: [],
+    }
+  },
+  methods: {
+    creataeTodo: function (todoTitle) {
+      const todo = {
+        title: todoTitle
+      }
+      this.todos.push(todo)
+      },
+      deleteTodo: function (todo) {
+        const index = this.todos.indexOf(todo)
+        this.todos.splice(index, 1)
+      }
+  }
+}
+</script>
+
+<style>
+
+  
+</style>
