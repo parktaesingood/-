c7929106d045e477454114fcb534c72fec68cc1b
parktaesingood <a01091976126@gmail.com>
2022-11-02 17:48:05 +0900

    1102

---
+++ 22_1102/vue-cli/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <!-- 보여주기 -->
+    <MyComponent/>
+    <HelloWorld message-title="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+// 1. 불러오기
+import MyComponent from './components/MyComponent.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    MyComponent,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ 22_1102/vue-cli/src/components/MyComponent.vue
@@ -0,0 +1,46 @@
+<template>
+<div class='border'>
+    <h1>싸피 이거는 내가 만든 새로운 컴포넌트!</h1>
+    <MyComponentItem
+     static-props="MyComponent에서 보낸 데이터"
+     v-bind:dynamic-props="dynamicProps"
+     @child-to-parent="parentGetEvent"
+     @child-input="getDynamicData "
+     />
+
+</div>
+  
+</template>
+
+<script>
+import MyComponentItem from '@/components/MyComponentItem'
+export default {
+    name: 'MyComponent',
+    components : {
+        MyComponentItem,
+    },
+    data: function () {
+        return{
+            dynamicProps: '이건 동적인 데이터!',
+        }
+    },
+    methods: {
+        parentGetEvent: function (childData) {
+            console.log('자식컴포넌트에서 보낸 data 들엇다')
+            console.log(childData)
+        },
+        getDynamicData: function (inputData) {
+            console.log('사용자가 입력한 값은 ${inputData}입니다.')
+            this.inputData = null
+        }
+    }
+
+}
+</script>
+
+<style>
+    .border {
+        border: solid 1px black;
+    }
+
+</style>
