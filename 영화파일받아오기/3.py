1612b1928f26af6c28ecdec39bc8711cd5287aee
parktaesingood <a01091976126@gmail.com>
2022-11-17 00:11:37 +0900

    1

--- 영화파일받아오기/3.py
+++ 영화파일받아오기/3.py
@@ -2,37 +2,38 @@ import requests
 import json
 
 
-def get_movie_datas():
+
+def get_genre_datas():
+    #1. URL 정보 설정
+    request_url_2 = "https://api.themoviedb.org/3/genre/movie/list"
+    params = {
+        'api_key' : 'fc4061dbc8efeb36b5ca0d5006427fd4',
+        "language" : "ko-KR",
+    }
     total_data = []
+    # 2. 요청 및 응답
+    genres = requests.get(request_url_2, params=params).json()
+    genres = genres['genres']
+    print(genres)
 
-    # 1페이지부터 500페이지까지 (페이지당 20개, 총 10,000개)
-    for i in range(1, 2):
-        request_url = f"https://api.themoviedb.org/3/movie/popular?api_key=fc4061dbc8efeb36b5ca0d5006427fd4&language=ko-KR&page={i}"
-        movies = requests.get(request_url).json()
-        print(movies['results'])
 
-        for movie in movies['results']:
-            if movie.get('release_date', ''):
-                fields = {
-                    'movie_id': movie['id'],
-                    'title': movie['title'],
-                    'released_date': movie['release_date'],
-                    'popularity': movie['popularity'],
-                    'vote_avg': movie['vote_average'],
-                    'overview': movie['overview'],
-                    'poster_path': movie['poster_path'],
-                    'genres': movie['genre_ids']
-                }
+    for genre in genres:
+        
+        fields = {
+            
+            # 'id': genre['id'],
+            'name': genre['name'],
+        }
 
-                data = {
-                    "pk": movie['id'],
-                    "model": "movies.movie",
-                    "fields": fields
-                }
+        data = {
+            "pk": genre['id'],
+            "model": "movies.genre",
+            "fields": fields
+        }
 
-                total_data.append(data)
+        total_data.append(data)
 
-    with open("movie_data.json", "w", encoding="utf-8") as w:
-        json.dump(total_data, w, indent="\\t", ensure_ascii=False)
+    with open("genre_data.json", "w", encoding="utf-8") as w:
+        json.dump(total_data, w, indent=4, ensure_ascii=False)
 
-get_movie_datas()
+get_genre_datas()
